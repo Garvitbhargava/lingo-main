12a2f542aad72225ee6b1c8d0a1369c7954c0a8c
garvitbhargava <garvitbhargava05@gmail.com>
2024-04-20 17:21:34 +0530

    update

---
+++ actions/user-progress.ts
@@ -0,0 +1,107 @@
+"use server";
+
+import db from "@/db/drizzle";
+import { getCourseById, getUserProgress } from "@/db/queries";
+import { challengeProgress, challenges, userProgress } from "@/db/schema";
+import { auth, currentUser } from "@clerk/nextjs";
+import { error } from "console";
+import { and, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const upsertUserProgress = async (courseId: number) => {
+  const { userId } = await auth();
+  const user = await currentUser();
+
+  if (!userId || !user) {
+    throw new Error("Unauthorized");
+  }
+
+  const course = await getCourseById(courseId);
+
+  if (!course) {
+    throw new Error("Course not found");
+  }
+  //TODO:Enable once units and lessons are added
+  //   if (!course.units.length || !course.units[0].lessons.length) {
+  //     throw new Error("Course is Empty");
+  //   }
+
+  const existingUserProgress = await getUserProgress();
+
+  if (existingUserProgress) {
+    await db.update(userProgress).set({
+      activeCourseId: courseId,
+      userName: user.firstName || "User",
+      userImageSrc: user.imageUrl || "/mascot.svg",
+    });
+
+    revalidatePath("courses");
+    revalidatePath("/learn");
+    redirect("/learn");
+  }
+
+  await db.insert(userProgress).values({
+    userId,
+    activeCourseId: courseId,
+    userName: user.firstName || "User",
+    userImageSrc: user.imageUrl || "/mascot.svg",
+  });
+
+  revalidatePath("courses");
+  revalidatePath("/learn");
+  redirect("/learn");
+};
+
+export const reduceHearts = async (challengeId: number) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const currentUserProgress = await getUserProgress();
+  //TODO: Get user subscription
+
+  const challenge = await db.query.challenges.findFirst({
+    where: eq(challenges.id, challengeId),
+  });
+  const lessonId = challenge.lessonId;
+  if (!challenge) {
+    throw new Error("Challenge not found");
+  }
+
+  const existingChallengeProgress = await db.query.challengeProgress.findFirst({
+    where: and(
+      eq(challengeProgress.userId, userId),
+      eq(challengeProgress.challengeId, challengeId)
+    ),
+  });
+  const isPractice = !!existingChallengeProgress;
+
+  if (isPractice) {
+    return { error: "practice" };
+  }
+  if (!currentUserProgress) {
+    throw new Error("User progress not found");
+  }
+
+  //TODO:Handel subscription
+
+  if (currentUserProgress.hearts === 0) {
+    return { error: "hearts" };
+  }
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: Math.max(currentUserProgress.hearts - 1, 0),
+    })
+    .where(eq(userProgress.userId, userId));
+
+  revalidatePath("/shop");
+  revalidatePath("/learn");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
+  revalidatePath(`/lesson/${lessonId}`);
+};
---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,76 @@
+import { redirect } from "next/navigation";
+
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+import {
+  getUnits,
+  getUserProgress,
+  getCourseProgress,
+  getLessonPercentage,
+} from "@/db/queries";
+import {
+  lessons,
+  userProgress,
+  units as unitsSchema,
+} from "../../../db/schema";
+import { Unit } from "./unit";
+
+const LearnPage = async () => {
+  const unitData = getUnits();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
+  const userProgressData = getUserProgress();
+
+  const [userProgress, units, courseProgress, lessonPercentage] =
+    await Promise.all([
+      userProgressData,
+      unitData,
+      courseProgressData,
+      lessonPercentageData,
+    ]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+  if (!courseProgress) {
+    redirect("/courses");
+  }
+  return (
+    <div className="flex flex-row-reverse gap-[48-px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={
+                courseProgress.activeLesson as
+                  | (typeof lessons.$inferSelect & {
+                      unit: typeof unitsSchema.$inferSelect;
+                    })
+                  | undefined
+              }
+              activeLessonPercentage={lessonPercentage}
+            />
+          </div>
+        ))}
+      </FeedWrapper>
+    </div>
+  );
+};
+
+export default LearnPage;
